992f38d03f0aacb856b48bd7dda6de2ee102960b
AleksandraBlomberg <146368482+AleksandraBlomberg@users.noreply.github.com>
2023-10-17 16:12:49 +0100

    complete employee.py

--- employee.py
+++ employee.py
@@ -1,20 +1,46 @@
 """Employee pay calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name, contract, hours, hourlyRate, salary, commission, bonusCommission, commissionNum, commissionPay):
-        self.name = name
-        self.contract = contract
-        self.hours = hours
-        self.salary = salary
-        self.hourlyRate = hourlyRate
+    def __init__(self):
+        self.name = ""
+        self.contract = False
+        self.hours = 0
+        self.salary = 0
+        self.hourlyRate = 0
         self.pay = 0
-        self.commission = commission
-        self.bonusCommission = bonusCommission
-        self.commissionNum = commissionNum
-        self.commissionPay = commissionPay
+        self.commission = False
+        self.bonusCommission = False
+        self.commissionNum = 0
+        self.commissionPay = 0
         self.payMethod = f"{self.name}"
 
+    def set_name(self,name):
+        self.name = name
+
+    def set_contract(self, contract):
+        self.contract = contract
+
+    def set_hours(self,hours):
+        self.hours = hours
+        
+    def set_salary(self,salary):
+        self.salary = salary
+
+    def set_hourlyRate(self,rate):
+        self.hourlyRate = rate
+
+    def set_commission(self,commission):
+        self.commission = commission
+
+    def set_bonusCommission(self,commission):
+        self.bonusCommission = commission
+
+    def set_commissionNum(self,num):
+        self.commissionNum = num
+
+    def set_commissionPay(self,pay):
+        self.commissionPay = pay
+    
     def commission_payout(self):
         if self.bonusCommission == True:
             self.payMethod = self.payMethod +f" and recives a bonus commission of {self.commissionPay}"
@@ -41,19 +67,48 @@ class Employee:
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie')
+billie = Employee()
+billie.set_name("Billie")
+billie.set_salary(4000)
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie')
+charlie = Employee()
+charlie.set_name("Charlie")
+charlie.set_contract(True)
+charlie.set_hours(100)
+charlie.set_hourlyRate(25)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee')
+renee = Employee()
+renee.set_name("Renne")
+renee.set_salary(3000)
+renee.set_commission(True)
+renee.set_commissionNum(4)
+renee.set_commissionPay(200)
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan')
+jan = Employee()
+jan.set_name("Jan")
+jan.set_contract(True)
+jan.set_hours(150)
+jan.set_hourlyRate(25)
+jan.set_commission(True)
+jan.set_commissionNum(3)
+jan.set_commissionPay(220)
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie')
+robbie = Employee()
+robbie.set_name("Robbie")
+robbie.set_salary(2000)
+robbie.set_bonusCommission(True)
+robbie.set_commissionPay(1500)
+
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel')
+ariel = Employee()
+ariel.set_name("Ariel")
+ariel.set_contract(True)
+ariel.set_hours(120)
+ariel.set_hourlyRate(30)
+ariel.set_bonusCommission(True)
+ariel.set_commissionPay(600)
